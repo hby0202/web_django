e7bde0fbb2abcbe8dd740180bef373001688806b
hby <hby0202@naver.com>
2024-06-21 14:55:29 +0900

    django web 기본 틀 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
 
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    if request.method == "POST":
+        # 파라미터
+        temp = request.POST.get('hello_world_input')
+
+        # DB 객체 연결
+        new_hello_world = HelloWorld()
+        # DB 컬럼 입력
+        new_hello_world.text = temp
+        # DB 저장
+        new_hello_world.save()
+
+        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        # url을 만들어주는 함수 :reverse
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
--- web_django/settings.py
+++ web_django/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'web_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#앱에 종속된 static(CSS, JavaScript, Images) 파일 경로
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
+
+#앱에 종속되지 않은 독립된 static(CSS, JavaScript, Images) 파일 경로
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
